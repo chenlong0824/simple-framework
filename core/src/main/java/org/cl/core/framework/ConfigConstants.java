df2f22b67cda22060ede6613033a101d0745a02a
LChen <cl.xiyou0804@163.com>
2018-09-09 16:15:32 +0800

    修改配置文件名称

--- core/src/main/java/org/cl/core/framework/ConfigConstants.java
+++ core/src/main/java/org/cl/core/framework/ConfigConstants.java
@@ -9,13 +9,13 @@ package org.cl.core.framework;
  */
 public interface ConfigConstants {
 
-    String CONFIG_FILE = "lw.properties";
-    String JDBC_DRIVER = "lw.framework.jdbc.driver";
-    String JDBC_URL = "lw.framework.jdbc.url";
-    String JDBC_USERNAME = "lw.framework.jdbc.username";
-    String JDBC_PASSWORD = "lw.framework.jdbc.password";
+    String CONFIG_FILE = "framework.properties";
+    String JDBC_DRIVER = "framework.jdbc.driver";
+    String JDBC_URL = "framework.jdbc.url";
+    String JDBC_USERNAME = "framework.jdbc.username";
+    String JDBC_PASSWORD = "framework.jdbc.password";
 
-    String APP_BASE_PACKAGE = "lw.framework.app.base_package";
-    String APP_JSP_PATH = "lw.framework.app.jsp_path";
-    String APP_ASSET_PATH = "lw.framework.app.asset_path";
+    String APP_BASE_PACKAGE = "framework.app.base_package";
+    String APP_JSP_PATH = "framework.app.jsp_path";
+    String APP_ASSET_PATH = "framework.app.asset_path";
 }
